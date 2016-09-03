5f55437bce8897c8099e0cd49e9bb95ab6f5b89e
Marina Hema <aussonia@gmail.com>
2016-09-03 13:01:02 -0300

    Agregado arch

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^huyamos/', include('polls.urls')),
+	url(r'^$', include('polls.urls')),
 ]
